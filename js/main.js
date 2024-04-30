84d99a2d93feffe7e157515bea832cac5068954c
Kermishkov1996 <gtytr.n@yandex.ru>
2024-04-30 12:44:43 +0400

    block scroll in open menu

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@ const menuBtn = document.querySelector('.menu__btn');
 const menuClose = document.querySelector('.menu__close');
 const menuList = document.querySelector('.menu__list');
 const menuShadow = document.querySelector('.menu__shadow');
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click', ()=>{
 	menuList.classList.toggle('menu__list--open');
 	menuShadow.classList.toggle('menu--open');
+	body.classList.add('menu--scroll');
 });
 
 menuClose.addEventListener('click', ()=>{
 	menuList.classList.remove('menu__list--open');
 	menuShadow.classList.remove('menu--open');
+	body.classList.remove('menu--scroll');
 });
+
+// document.querySelector('.menu__link').addEventListener('click', ()=>{
+// 	menuList.classList.remove('menu__list--open');
+// 	menuShadow.classList.remove('menu--open');
+// 	body.classList.remove('menu--scroll');
+// });
