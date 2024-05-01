94af880d98d452af6926dea31f3bd41f928200d8
Kermishkov1996 <gtytr.n@yandex.ru>
2024-05-01 19:05:28 +0400

    add func for close menu

--- js/main.js
+++ js/main.js
@@ -4,20 +4,31 @@ const menuList = document.querySelector('.menu__list');
 const menuShadow = document.querySelector('.menu__shadow');
 const body = document.querySelector('body');
 
+//открывает меню при нажатии на бургер
 menuBtn.addEventListener('click', ()=>{
 	menuList.classList.toggle('menu__list--open');
 	menuShadow.classList.toggle('menu--open');
 	body.classList.add('menu--scroll');
 });
 
-menuClose.addEventListener('click', ()=>{
+//функция для закрытия меню
+function close() {
 	menuList.classList.remove('menu__list--open');
 	menuShadow.classList.remove('menu--open');
 	body.classList.remove('menu--scroll');
+}
+
+//закрывает меню при нажатии на крестик в меню
+menuClose.addEventListener('click', ()=>{
+	close();
 });
 
-// document.querySelector('.menu__link').addEventListener('click', ()=>{
-// 	menuList.classList.remove('menu__list--open');
-// 	menuShadow.classList.remove('menu--open');
-// 	body.classList.remove('menu--scroll');
-// });
+//закрывает меню при нажатии на ссылку в меню
+menuList.addEventListener('click', (event)=>{
+	if(event.target.tagName === "A")
+	close();
+});
+
+//закрывает меню при нажатии на поле за пределами меню
+menuShadow.addEventListener('click', close);
+
